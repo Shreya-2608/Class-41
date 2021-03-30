21f9c2ebd27958dde5c6506fc25e43e4f68d3420
Shreya-2608 <shreya.kalluri2608@gmail.com>
2021-03-30 14:48:31 +0530

    Modified

--- sketch.js
+++ sketch.js
@@ -12,13 +12,16 @@ var cars, car1, car2, car3, car4;
 
 var track, car1_img, car2_img, car3_img, car4_img;
 
+var BackgroundSound;
+
 function preload(){
-  track = loadImage("../images/track.jpg");
-  car1_img = loadImage("../images/car1.png");
-  car2_img = loadImage("../images/car2.png");
-  car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
+  track = loadImage("images/track.jpg");
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+  ground = loadImage("images/ground.png");
+  BackgroundSound = loadSound("BackgroundSound.mp3");
 }
 
 function setup(){
@@ -27,6 +30,7 @@ function setup(){
   game = new Game();
   game.getState();
   game.start();
+  BackgroundSound.play();
 }
 
 
